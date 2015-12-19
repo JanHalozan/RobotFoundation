d3caec58b953f1585c457d31f02a065708709744
Matt Rajca <matt.rajca@me.com>
2015-12-19 11:29:27 -0600

    Migrated the Delete command

---
+++ Sources/NXTDeleteCommand.swift
@@ -0,0 +1,24 @@
+//
+//  NXTDeleteCommand.swift
+//  RobotFoundation
+//
+//  Created by Matt on 12/19/15.
+//
+
+import Foundation
+
+struct NXTDeleteCommand: NXTCommand {
+	let filename: String
+
+	var type: NXTCommandType {
+		return .System
+	}
+
+	var identifier: UInt8 {
+		return 0x85
+	}
+
+	var payloadData: NSData {
+		return (filename as NSString).dataForFilename
+	}
+}
