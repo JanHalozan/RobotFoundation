9181c619fb7240ab8f62da130eadbfa483279999
Matt Rajca <matt.rajca@me.com>
2015-12-19 10:52:29 -0600

    Added a barebones Cocoa framework project

---
+++ RobotFoundation.h
@@ -0,0 +1,19 @@
+//
+//  RobotFoundation.h
+//  RobotFoundation
+//
+//  Created by Matt on 12/19/15.
+//  Copyright © 2015 Matt Rajca. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for RobotFoundation.
+FOUNDATION_EXPORT double RobotFoundationVersionNumber;
+
+//! Project version string for RobotFoundation.
+FOUNDATION_EXPORT const unsigned char RobotFoundationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RobotFoundation/PublicHeader.h>
+
+
