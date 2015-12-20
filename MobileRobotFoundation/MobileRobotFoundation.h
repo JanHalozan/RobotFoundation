a710f0ce3d7d8f459cdcea76abbbe71fa57ae5f5
Matt Rajca <matt.rajca@me.com>
2015-12-19 21:12:37 -0600

    Added a MobileRobotFoundation target for iOS support

---
+++ MobileRobotFoundation/MobileRobotFoundation.h
@@ -0,0 +1,19 @@
+//
+//  MobileRobotFoundation.h
+//  MobileRobotFoundation
+//
+//  Created by Matt on 12/19/15.
+//  Copyright © 2015 Matt Rajca. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MobileRobotFoundation.
+FOUNDATION_EXPORT double MobileRobotFoundationVersionNumber;
+
+//! Project version string for MobileRobotFoundation.
+FOUNDATION_EXPORT const unsigned char MobileRobotFoundationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MobileRobotFoundation/PublicHeader.h>
+
+
