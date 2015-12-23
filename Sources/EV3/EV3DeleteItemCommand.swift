9049540966a81cbab8d4cd9e853f94d463c4adef
Matt Rajca <matt.rajca@me.com>
2015-12-23 16:30:59 -0600

    Added a command for deleting files

---
+++ Sources/EV3/EV3DeleteItemCommand.swift
@@ -0,0 +1,31 @@
+//
+//  EV3DeleteItemCommand.swift
+//  RobotFoundation
+//
+//  Created by Matt on 12/23/15.
+//
+
+import Foundation
+
+public struct EV3DeleteItemCommand: EV3SystemCommand {
+	public let path: String
+
+	public init(path: String) {
+		self.path = path
+	}
+
+	public var responseType: MindstormsResponse.Type {
+		return EV3GenericResponse.self
+	}
+
+	public var systemCommand: UInt8 {
+		return 0x9C
+	}
+
+	public var payloadData: NSData {
+		let mutableData = NSMutableData()
+		mutableData.appendString(path)
+
+		return mutableData.copy() as! NSData
+	}
+}
