631a94d08711422614c618a3863db1008a9b9ade
Matt Rajca <matt.rajca@me.com>
2016-01-08 16:47:29 -0600

    Added convenience API for EV3 file uploads

--- Sources/EV3/EV3DeviceConvenience.swift
+++ Sources/EV3/EV3DeviceConvenience.swift
@@ -8,9 +8,35 @@
 import Foundation
 
 public typealias EV3DeviceDownloadHandler = NSData -> ()
+public typealias EV3DeviceUploadHandler = Bool -> ()
 
 // Offers convenience API for "sequential" commands.
 extension EV3Device {
+	public func uploadFileData(data: NSData, toPath path: String, handler: EV3DeviceUploadHandler) {
+		let command = EV3UploadFileCommand(path: path, bytesToWrite: UInt32(data.length))
+		enqueueCommand(command) { response in
+			let handleResponse = response as! EV3HandleResponse
+
+			self.continueFileUploadWithHandler(handleResponse.handle, dataLeft: data, handler: handler)
+		}
+	}
+
+	private func continueFileUploadWithHandler(handle: UInt8, dataLeft: NSData, handler: EV3DeviceUploadHandler) {
+		let chunk = dataLeft.length > 1000 ? dataLeft.subdataWithRange(NSMakeRange(0, 1000)) : dataLeft
+		let moreToUpload = dataLeft.length > 1000
+
+		let command = EV3ContinueUploadFileCommand(handle: handle, data: chunk)
+		enqueueCommand(command) { response in
+
+			if moreToUpload {
+				let dataLeft = dataLeft.subdataWithRange(NSMakeRange(1000, dataLeft.length - 1000))
+				self.continueFileUploadWithHandler(handle, dataLeft: dataLeft, handler: handler)
+			} else {
+				handler(true)
+			}
+		}
+	}
+
 	public func downloadFileAtPath(path: String, handler: EV3DeviceDownloadHandler) {
 		let command = EV3ReadFileCommand(path: path, bytesToRead: 1000)
 		enqueueCommand(command) { response in
