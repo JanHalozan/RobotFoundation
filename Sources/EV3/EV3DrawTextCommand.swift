d90c3610989c177dfea74313fa6928ca55e08d8b
Matt Rajca <matt.rajca@me.com>
2016-02-03 10:23:21 -0600

    Font size selection should happen as part of the draw text command

--- Sources/EV3/EV3DrawTextCommand.swift
+++ Sources/EV3/EV3DrawTextCommand.swift
@@ -7,17 +7,25 @@
 
 import Foundation
 
+public enum EV3FontSizeConst: UInt8 {
+	case Small = 0
+	case Medium = 1
+	case Large = 2
+}
+
 public struct EV3DrawTextCommand: EV3DirectCommand {
 	private let color: EV3FillColorConst
 	private let x: UInt16
 	private let y: UInt16
 	private let string: String
+	private let fontSize: EV3FontSizeConst
 
-	public init(color: EV3FillColorConst, x: UInt16, y: UInt16, string: String) {
+	public init(color: EV3FillColorConst, x: UInt16, y: UInt16, string: String, fontSize: EV3FontSizeConst) {
 		self.color = color
 		self.x = x
 		self.y = y
 		self.string = string
+		self.fontSize = fontSize
 	}
 
 	public var responseType: MindstormsResponse.Type {
@@ -26,6 +34,9 @@ public struct EV3DrawTextCommand: EV3DirectCommand {
 
 	public var payloadData: NSData {
 		let mutableData = NSMutableData()
+		mutableData.appendUInt8(EV3OpCode.UIDraw.rawValue)
+		mutableData.appendUInt8(EV3UIDrawOpSubcode.SelectFont.rawValue)
+		mutableData.appendUInt8(fontSize.rawValue)
 		mutableData.appendUInt8(EV3OpCode.UIDraw.rawValue)
 		mutableData.appendUInt8(EV3UIDrawOpSubcode.Text.rawValue)
 		mutableData.appendUInt8(color.rawValue)
