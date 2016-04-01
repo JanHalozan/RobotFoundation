dc3588d013edbce465f8499027888b40bfa03184
Matt Rajca <matt.rajca@me.com>
2016-04-01 11:31:52 -0500

    Exposed NXTPlaySoundFileCommand

--- Sources/NXT/NXTPlaySoundFileCommand.swift
+++ Sources/NXT/NXTPlaySoundFileCommand.swift
@@ -7,23 +7,28 @@
 
 import Foundation
 
-struct NXTPlaySoundFileCommand: NXTCommand {
-	let loop: Bool
-	let filename: String
+public struct NXTPlaySoundFileCommand: NXTCommand {
+	public let loop: Bool
+	public let filename: String
 
-	var responseType: MindstormsResponse.Type {
+	public init(filename: String, loop: Bool) {
+		self.filename = filename
+		self.loop = loop
+	}
+
+	public var responseType: MindstormsResponse.Type {
 		return NXTGenericResponse.self
 	}
 
-	var type: MindstormsCommandType {
+	public var type: MindstormsCommandType {
 		return .Direct
 	}
 
-	var identifier: UInt8 {
+	public var identifier: UInt8 {
 		return 0x02
 	}
 
-	var payloadData: NSData {
+	public var payloadData: NSData {
 		let data = NSMutableData()
 		data.appendUInt8(loop ? 1 : 0)
 		data.appendNXTFilename(filename)
