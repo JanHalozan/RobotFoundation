16a9643e204a2af4a43cf7e6ecb7fb24c1cc4981
Matt Rajca <matt.rajca@me.com>
2016-04-01 11:41:43 -0500

    Added NXT stop sound playback command

---
+++ Sources/NXT/NXTStopPlaybackCommand.swift
@@ -0,0 +1,28 @@
+//
+//  NXTStopPlaybackCommand.swift
+//  RobotFoundation
+//
+//  Created by Matt on 4/1/16.
+//
+
+import Foundation
+
+public struct NXTStopPlaybackCommand: NXTCommand {
+	public init() { }
+
+	public var responseType: MindstormsResponse.Type {
+		return NXTGenericResponse.self
+	}
+
+	public var type: MindstormsCommandType {
+		return .Direct
+	}
+
+	public var identifier: UInt8 {
+		return 0x0C
+	}
+
+	public var payloadData: NSData {
+		return NSData()
+	}
+}
