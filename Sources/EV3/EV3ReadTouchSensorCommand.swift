520f93fe79bb8a839025d39a07d49a87b6797e70
Matt Rajca <matt.rajca@me.com>
2016-05-22 17:42:09 +0200

    EV3ReadTouchSensorCommand now returns Boolean values

--- Sources/EV3/EV3ReadTouchSensorCommand.swift
+++ Sources/EV3/EV3ReadTouchSensorCommand.swift
@@ -7,11 +7,14 @@
 
 import Foundation
 
+// 1 if on, 0 if off.
+private let kEV3TouchSensorBooleanMode = UInt8(0)
+
 public struct EV3ReadTouchSensorCommand: EV3DirectCommand {
 	public let port: EV3InputPort
 
 	public var responseType: MindstormsResponse.Type {
-		return EV3PercentByteResponse.self
+		return EV3BooleanResponse.self
 	}
 
 	public var globalSpaceSize: UInt16 {
@@ -20,12 +23,12 @@ public struct EV3ReadTouchSensorCommand: EV3DirectCommand {
 
 	public var payloadData: NSData {
 		let mutableData = NSMutableData()
-		mutableData.appendUInt8(EV3OpCode.InputRead.rawValue)
+		mutableData.appendUInt8(EV3OpCode.InputReadSI.rawValue)
 		mutableData.appendUInt8(EV3Layer.ThisBrick.rawValue)
 		mutableData.appendUInt8(port.rawValue)
 
 		mutableData.appendUInt8(EV3SensorType.KeepType.rawValue)
-		mutableData.appendUInt8(0) // Touch mode
+		mutableData.appendUInt8(kEV3TouchSensorBooleanMode)
 		mutableData.appendUInt8(EV3Variables.GlobalVar0.rawValue)
 
 		return mutableData.copy() as! NSData
