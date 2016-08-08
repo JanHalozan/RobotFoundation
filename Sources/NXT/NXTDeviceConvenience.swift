6ab9a9091fae270303061de41af0377538ee33ca
Matt Rajca <matt.rajca@me.com>
2016-08-08 10:43:28 -0500

    Improved error handling in NXTDeviceConvenience

--- Sources/NXT/NXTDeviceConvenience.swift
+++ Sources/NXT/NXTDeviceConvenience.swift
@@ -19,44 +19,61 @@ public typealias NXTDeviceUploadHandler = (Bool) -> ()
 
 // Offers convenience API for "sequential" commands.
 extension NXTDevice {
-	public func readDisplay(handler: NSData -> ()) {
+	public func readDisplay(handler: NSData? -> ()) {
 		let allData = NSMutableData()
+		var allGood = true
 
 		for n: UInt16 in 0..<16 {
 			let command = NXTReadIOMapCommand(module: kDisplayModule, offset: kDisplayNormalOffset + (n * kMaxBytes), bytesToRead: kMaxBytes)
-			enqueueCommand(command) { response in
-				guard let nxtResponse = response as? NXTIOMapResponse else {
-					assertionFailure()
-					return
-				}
+			enqueueCommand(command) { result in
+				assert(NSThread.isMainThread())
 
-				allData.appendData(nxtResponse.contents)
+				switch result {
+				case .Error:
+					allGood = false
+				case .Response(let response):
+					guard let nxtResponse = response as? NXTIOMapResponse else {
+						allGood = false
+						assertionFailure()
+						break
+					}
 
-				if allData.length == kNXTScreenHeight/8 * kNXTScreenWidth {
-					handler(allData)
+					allData.appendData(nxtResponse.contents)
 				}
+			}
+		}
+
+		enqueueBarrier {
+			if allGood {
+				handler(allData)
+			} else {
+				handler(nil)
 			}
 		}
 	}
 
 	public func uploadFileData(wholeData: NSData, toPath path: String, handler: NXTDeviceUploadHandler) {
 		let openCommand = NXTOpenWriteCommand(filename: path, size: UInt32(wholeData.length))
-		enqueueCommand(openCommand) { response in
-			guard let handleResponse = response as? NXTHandleResponse else {
-				assertionFailure()
-				return
-			}
+		enqueueCommand(openCommand) { result in
+			assert(NSThread.isMainThread())
 
-			if handleResponse.status == .StatusSuccess {
-				self.actuallyUploadFileData(wholeData, toFileAtHandle: handleResponse.handle, handler: handler)
-			} else {
+			switch result {
+			case .Error:
 				handler(false)
+			case .Response(let response):
+				guard let handleResponse = response as? NXTHandleResponse else {
+					handler(false)
+					assertionFailure()
+					return
+				}
+
+				self.actuallyUploadFileData(wholeData, toFileAtHandle: handleResponse.handle, handler: handler)
 			}
 		}
 	}
 
 	private func actuallyUploadFileData(wholeData: NSData, toFileAtHandle handle: UInt8, handler: NXTDeviceUploadHandler) {
-		var anyFailed = false
+		var allGood = true
 		var dataLeft = wholeData
 
 		while dataLeft.length > 0 {
@@ -70,14 +87,18 @@ extension NXTDevice {
 			}
 
 			let command = NXTWriteCommand(handle: handle, contents: chunk)
-			enqueueCommand(command) { response in
-				guard let handleResponse = response as? NXTHandleSizeResponse else {
-					assertionFailure()
-					return
-				}
-				
-				if handleResponse.status != .StatusSuccess {
-					anyFailed = true
+			enqueueCommand(command) { result in
+				assert(NSThread.isMainThread())
+
+				switch result {
+				case .Error:
+					allGood = false
+				case .Response(let response):
+					guard let _ = response as? NXTHandleSizeResponse else {
+						allGood = false
+						assertionFailure()
+						break
+					}
 				}
 			}
 		}
@@ -85,20 +106,27 @@ extension NXTDevice {
 		closeHandle(handle)
 
 		enqueueBarrier {
-			handler(!anyFailed)
+			handler(allGood)
 		}
 	}
 
 	public func downloadFileAtPath(path: String, handler: NXTDeviceDownloadHandler) {
 		let command = NXTOpenReadCommand(filename: path)
-		enqueueCommand(command) { response in
-			let openResponse = response as! NXTHandleSizeResponse
+		enqueueCommand(command) { result in
+			assert(NSThread.isMainThread())
 
-			if openResponse.status == .StatusSuccess {
+			switch result {
+			case .Error:
+				handler(nil)
+			case .Response(let response):
+				guard let openResponse = response as? NXTHandleSizeResponse else {
+					handler(nil)
+					assertionFailure()
+					break
+				}
+
 				// We finished reading but there is more!
 				self.continueFileDownloadWithHandle(openResponse.handle, bytesLeft: openResponse.size, dataSoFar: NSData(), handler: handler)
-			} else {
-				assertionFailure()
 			}
 		}
 	}
@@ -106,11 +134,21 @@ extension NXTDevice {
 	private func continueFileDownloadWithHandle(handle: UInt8, bytesLeft: UInt16, dataSoFar: NSData, handler: EV3DeviceDownloadHandler) {
 		let bytesToRead = bytesLeft > UInt16(kMaxChunk) ? UInt16(kMaxChunk) : UInt16(bytesLeft)
 		let continueCommand = NXTReadCommand(handle: handle, bytesToRead: bytesToRead)
-		enqueueCommand(continueCommand) { continueResponse in
-			let dataResponse = continueResponse as! NXTDataResponse
-			let newDataSoFar = dataSoFar.dataByAppendingData(dataResponse.contents)
+		enqueueCommand(continueCommand) { result in
+			assert(NSThread.isMainThread())
 
-			if dataResponse.status == .StatusSuccess {
+			switch result {
+			case .Error:
+				handler(nil)
+			case .Response(let response):
+				guard let dataResponse = response as? NXTDataResponse else {
+					handler(nil)
+					assertionFailure()
+					break
+				}
+
+				let newDataSoFar = dataSoFar.dataByAppendingData(dataResponse.contents)
+
 				let newBytesLeft = bytesLeft - bytesToRead
 				if newBytesLeft == 0 {
 					handler(newDataSoFar)
@@ -119,22 +157,26 @@ extension NXTDevice {
 					// We finished reading but there is more!
 					self.continueFileDownloadWithHandle(handle, bytesLeft: newBytesLeft, dataSoFar: newDataSoFar, handler: handler)
 				}
-			} else {
-				assertionFailure()
 			}
 		}
 	}
 
 	private func closeHandle(handle: UInt8) {
 		let closeCommand = NXTCloseCommand(handle: handle)
-		enqueueCommand(closeCommand) { response in
-			guard let genericResponse = response as? NXTGenericResponse else {
-				assertionFailure()
-				return
-			}
+		enqueueCommand(closeCommand) { result in
+			assert(NSThread.isMainThread())
 
-			if genericResponse.status != .StatusSuccess {
-				debugPrint("Closing the file handle failed (\(genericResponse.status))")
+			switch result {
+			case .Error:
+				// If it failed there's nothing we can do.
+				break
+			case .Response(let response):
+				guard let genericResponse = response as? NXTGenericResponse else {
+					assertionFailure()
+					break
+				}
+
+				print("Closing the file handle failed (\(genericResponse.status))")
 			}
 		}
 	}
