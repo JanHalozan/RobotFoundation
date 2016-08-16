2b55e7371cd7f3ceead826284e878b5d3170c50c
Matt Rajca <matt.rajca@me.com>
2016-08-15 19:55:58 -0500

    Don't check the transport state when enqueing NXT operations as connections are made on-demand

--- Sources/NXT/NXTDevice.swift
+++ Sources/NXT/NXTDevice.swift
@@ -10,12 +10,6 @@ import Foundation
 public final class NXTDevice: Device {
 	// The device will wait until all critical commands complete before going away.
 	public func enqueueCommand(command: NXTCommand, isCritical: Bool = true, responseHandler: NXTCommandHandler) {
-		if transport.openState.get() == .Closed {
-			print("No open transport, won't bother enqueuing the command.")
-			responseHandler(.Error(.TransportError(kIOReturnAborted)))
-			return
-		}
-
 		let operation = NXTCommandOperation(transport: transport, command: command, isCritical: isCritical, responseHandler: responseHandler)
 		enqueueOperation(operation)
 	}
