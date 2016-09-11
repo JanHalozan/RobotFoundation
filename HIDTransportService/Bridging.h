61c10f31568d30605c99ee8eb1bfe239c3e6d1c1
Matt Rajca <matt.rajca@me.com>
2016-09-11 14:55:42 -0500

    Import our NSMachBootstrapServer wrapper in the bridging header

---
+++ HIDTransportService/Bridging.h
@@ -0,0 +1,13 @@
+//
+//  Bridging.h
+//  RobotFoundation
+//
+//  Created by Matt on 9/11/16.
+//
+
+#ifndef Bridging_h
+#define Bridging_h
+
+#import "RBMachBootstrapServer.h"
+
+#endif /* Bridging_h */
