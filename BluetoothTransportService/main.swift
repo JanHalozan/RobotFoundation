3be915e548a3d941108979552c1b6efed8591294
Matt Rajca <matt.rajca@me.com>
2016-09-11 16:56:25 -0500

    Updated project for Mach-based BluetoothTransportService

--- BluetoothTransportService/main.swift
+++ BluetoothTransportService/main.swift
@@ -5,7 +5,7 @@
 //  Created by Matt on 12/26/15.
 //
 
-let server = MachServer(name: "RJKYY38TY2.com.Robotary.HID", transportServiceType: BluetoothTransportService.self)
+let server = MachServer(name: "RJKYY38TY2.com.Robotary.Bluetooth", transportServiceType: BluetoothTransportService.self)
 
 // Incoming connections are processed on a new thread which this creates (matches XPC).
 server.run()
