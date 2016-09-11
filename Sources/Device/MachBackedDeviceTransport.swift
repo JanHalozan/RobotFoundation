b7913132ad56d02eaa806085236784efd02884c0
Matt Rajca <matt.rajca@me.com>
2016-09-11 15:37:46 -0500

    Added a Mach-backed device transport service

---
+++ Sources/Device/MachBackedDeviceTransport.swift
@@ -0,0 +1,213 @@
+//
+//  MachBackedDeviceTransport.swift
+//  RobotFoundation
+//
+//  Created by Matt on 9/11/16.
+//
+
+#if os(OSX)
+
+import Foundation
+import IOKit.hid
+
+private final class MachDelegate: NSObject, NSMachPortDelegate {
+	let handler: (PortMessage) -> ()
+
+	init(handler: @escaping (PortMessage) -> ()) {
+		self.handler = handler
+	}
+
+	func handle(_ message: PortMessage) {
+		handler(message)
+	}
+}
+
+class MachBackedDeviceTransport: DeviceTransport, TransportClientProtocol {
+	// Services can talk back to us on this port.
+	private let clientPort: NSMachPort
+	private let serverQueue = DispatchQueue(label: "server")
+	private lazy var machDelegate: MachDelegate = { [unowned self] in
+		return MachDelegate(handler: self.handleMessage)
+	}()
+
+	private var serverConnection: NSMachPort?
+
+	override init() {
+		clientPort = NSMachPort()
+		super.init()
+
+		// We need to ensure we schedule this on the main queue.
+		assert(Thread.isMainThread)
+		clientPort.setDelegate(self.machDelegate)
+		clientPort.schedule(in: .current, forMode: .commonModes)
+
+		NotificationCenter.default.addObserver(self, selector: #selector(portBecameInvalid), name: Port.didBecomeInvalidNotification, object: nil)
+	}
+
+	deinit {
+		// This may not be a weak reference...
+		clientPort.setDelegate(nil)
+		NotificationCenter.default.removeObserver(self)
+	}
+
+	@objc private func portBecameInvalid(note: Notification) {
+		assert(Thread.isMainThread)
+
+		guard let port = note.object as? Port else {
+			assertionFailure()
+			return
+		}
+
+		assert(port !== clientPort)
+
+		serverQueue.async {
+			if port === self.serverConnection {
+				self.serverConnection = nil
+			}
+		}
+	}
+
+	var serviceName: String {
+		fatalError("Subclasses must override this method")
+	}
+
+	var executableName: String {
+		fatalError("Subclasses must override this method")
+	}
+
+	var identifier: String {
+		fatalError("Subclasses must override this method")
+	}
+
+	func handleMessage(_ message: PortMessage) {
+		assert(Thread.isMainThread)
+
+		guard let data = message.components?.first as? Data else {
+			assertionFailure()
+			print("\(#function): no data to read")
+			return
+		}
+
+		guard let plist = try? PropertyListSerialization.propertyList(from: data, options: PropertyListSerialization.ReadOptions(rawValue: 0), format: nil) else {
+			assertionFailure()
+			print("\(#function): could not de-serialize the request")
+			return
+		}
+
+		guard let request = plist as? [String: AnyObject] else {
+			assertionFailure()
+			print("\(#function): unexpected format")
+			return
+		}
+
+		guard let type = request[MachEventKey.type.rawValue] as? String else {
+			assertionFailure()
+			print("\(#function): no request type")
+			return
+		}
+
+		guard let parsedType = MachResponseType(rawValue: type) else {
+			assertionFailure()
+			print("\(#function): unknown type")
+			return
+		}
+
+		switch parsedType {
+		case .receivedData:
+			guard let packetData = request[MachEventKey.data.rawValue] as? NSData else {
+				assertionFailure()
+				print("\(#function): no packet data")
+				return
+			}
+
+			handleTransportData(packetData)
+		case .closedConnection:
+			handleClosedConnection()
+		}
+	}
+
+	private func openConnectionIfNecessary() {
+		var needsToConnect = false
+		serverQueue.sync {
+			needsToConnect = serverConnection == nil
+			assert(serverConnection?.isValid ?? true)
+		}
+
+		guard needsToConnect else {
+			return
+		}
+
+		if RBMachBootstrapServer.port(withName: serviceName) == nil {
+			// Launch the service first.
+			let task = Process()
+			let bundle = Bundle(for: MachDelegate.self)
+			task.launchPath = bundle.bundleURL.appendingPathComponent("Versions/A/\(executableName)").path
+			task.launch()
+
+			// Wait for the port to become available.
+			// This is usually called on a background thread so blocking for a bit is okay.
+			while RBMachBootstrapServer.port(withName: serviceName) == nil {
+				Thread.sleep(forTimeInterval: 0.1)
+			}
+		}
+
+		guard let port = RBMachBootstrapServer.port(withName: serviceName) else {
+			assertionFailure()
+			return
+		}
+
+		serverQueue.sync {
+			self.serverConnection = port
+		}
+
+		let packet = [
+			MachEventKey.type.rawValue: MachRequestType.openConnection.rawValue as NSString
+		]
+		sendPacket(packet)
+	}
+
+	private func sendPacket(_ packet: [String: AnyObject]) {
+		openConnectionIfNecessary()
+
+		guard let packetData = try? PropertyListSerialization.data(fromPropertyList: packet, format: .binary, options: 0) else {
+			print("\(#function): could not serialize packet")
+			assertionFailure()
+			return
+		}
+
+		serverQueue.async {
+			assert(self.serverConnection != nil)
+			let message = PortMessage(send: self.serverConnection, receive: self.clientPort, components: [packetData])
+			if !message.send(before: .distantFuture) {
+				print("\(#function): the message could not be sent")
+			}
+		}
+	}
+
+	override func writeData(_ data: Data, errorHandler: @escaping (Error) -> ()) throws {
+		let packet = [
+			MachEventKey.type.rawValue: MachRequestType.writeData.rawValue as NSString,
+			MachEventKey.data.rawValue: data as NSData,
+			MachEventKey.identifier.rawValue: identifier as NSString
+		]
+		sendPacket(packet)
+	}
+
+	override func scheduleRead() {
+		let packet = [
+			MachEventKey.type.rawValue: MachRequestType.scheduleRead.rawValue as NSString,
+			MachEventKey.identifier.rawValue: identifier as NSString
+		]
+		sendPacket(packet)
+	}
+
+	@objc func handleTransportData(_ data: NSData) {
+		self.handleData(data as Data)
+	}
+	
+	@objc func closedTransportConnection() {
+		self.handleClosedConnection()
+	}
+}
+
+#endif
