81fd5fe34ba98532e0289d9d500f26981562d0e0
Matt Rajca <matt.rajca@me.com>
2016-09-11 17:00:17 -0500

    Get LegacyUSBTransportService ready for Mach backend

--- LegacyUSBTransportService/main.swift
+++ LegacyUSBTransportService/main.swift
@@ -5,52 +5,10 @@
 //  Created by Matt on 6/29/16.
 //
 
-import Foundation
+let server = MachServer(name: "RJKYY38TY2.com.Robotary.Legacy", transportServiceType: LegacyUSBTransportService.self)
 
-final class ServiceDelegate : NSObject, NSXPCListenerDelegate, LegacyUSBTransportServiceDelegate {
-	private lazy var exportedObject: LegacyUSBTransportService = {
-		LegacyUSBTransportService(delegate: self)
-	}()
+// Incoming connections are processed on a new thread which this creates (matches XPC).
+server.run()
 
-	private var connections = Set<NSXPCConnection>()
-
-	func listener(_ listener: NSXPCListener, shouldAcceptNewConnection newConnection: NSXPCConnection) -> Bool {
-		connections.insert(newConnection)
-
-		newConnection.exportedInterface = NSXPCInterface(with: TransportServiceProtocol.self)
-		newConnection.remoteObjectInterface = NSXPCInterface(with: TransportClientProtocol.self)
-		newConnection.exportedObject = exportedObject
-		newConnection.invalidationHandler = { [unowned self] in
-			DispatchQueue.main.async {
-				self.connections.remove(newConnection)
-
-				if self.connections.isEmpty {
-					// Exit if no more connections...
-					exit(0)
-				}
-			}
-		}
-		newConnection.resume()
-
-		return true
-	}
-
-	func handle(_ data: Data) {
-		for connection in connections {
-			if let client = connection.remoteObjectProxy as? TransportClientProtocol {
-				client.handleTransportData(data as NSData)
-			} else {
-				assertionFailure()
-			}
-		}
-	}
-}
-
-let delegate = ServiceDelegate()
-
-let listener = NSXPCListener.service()
-listener.delegate = delegate
-
-// This method does not return.
-listener.resume()
-
+// The hardware code is processed on the main thread.
+RunLoop.current.run()
